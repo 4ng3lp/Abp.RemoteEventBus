9bb9c5fd09a49ee86f175220284f53a9e291fd7d
Evgeni Zaplatin <zefslab@gmail.com>
2019-12-24 09:23:58 +0700

    dev - рефакторинг и документирование класса AttributeRemoteEventHandler  регистрирующего всех обработчиков в приложении

--- src/Abp.RemoteEventBus/Impl/AttributeRemoteEventHandler.cs
+++ src/Abp.RemoteEventBus/Impl/AttributeRemoteEventHandler.cs
@@ -10,13 +10,26 @@ using Abp.RemoteEventBus.Exceptions;
 
 namespace Abp.RemoteEventBus.Impl
 {
+    /// <summary>
+    /// Задача этого синглтона при создании сформировать маппинг на основе
+    /// зарегистрированных в системе обработчиков сообщений от Кафка.
+    /// А при срабатывании метода HandleEvent, определяется тип сообщения и запускается
+    /// подходящие обработчики, выбирая из из мапинга
+    /// </summary>
     public class AttributeRemoteEventHandler : IEventHandler<RemoteEventArgs>, ISingletonDependency
     {
         public ILogger Logger { get; set; }
 
         private readonly TypeFinder _typeFinder;
 
-        private readonly Dictionary<string, List<Tuple<Type, RemoteEventHandlerAttribute>>> _typeMapping;
+        /// <summary>
+        /// Содержит маппинг типов данных в сообщениях от Кафка на типы хендлеров с их атрибутами.
+        /// В качестве ключа используется тип данных,
+        /// а в качестве значения - список пар: Тип хендлера и его атрибут.
+        /// Таким образом на сообщения с одним типом данных может быть зарегистрировано несколько
+        /// обработчиков
+        /// </summary>
+        private readonly Dictionary<string, List<(Type handlerType, RemoteEventHandlerAttribute attribute)>> _typeMapping;
 
         private readonly IIocResolver _iocResolver;
 
@@ -30,48 +43,61 @@ namespace Abp.RemoteEventBus.Impl
             _iocResolver = iocResolver;
             _eventBus = eventBus;
 
-            _typeMapping = new Dictionary<string, List<Tuple<Type, RemoteEventHandlerAttribute>>>();
+            _typeMapping = new Dictionary<string, List<(Type handlerType, RemoteEventHandlerAttribute attribute)>>();
 
-            //Перечень типов, на которые повешаны атрибуты RemoteEventHandlerAttribute
+            //Перечень типов-обработчиков, на которые повешаны атрибуты RemoteEventHandlerAttribute
             var typeList = _typeFinder.Find(type =>
                 Attribute.IsDefined(type, typeof(RemoteEventHandlerAttribute), false) &&
                 typeof(IRemoteEventHandler).IsAssignableFrom(type));
+            
             foreach(var type in typeList)
             {
                 ///TODO проблема в том, что на отдельных тапах могут весеть более одного атрибута
                 /// что собственно не запрещено в описании атрибута
+                /// поэтому нужно изменить код так, чтобы можно было регистрировать обработчики
+                /// с несколькими атрибутами
                     var attribute = Attribute.GetCustomAttribute(type, typeof(RemoteEventHandlerAttribute)) as RemoteEventHandlerAttribute;
+                    //обработчик относится к данным определенного типа
                     var key = attribute.ForType;
-                    var item = new Tuple<Type, RemoteEventHandlerAttribute>(type, attribute);
+                    var item = (handlerTyp: type, attribute : attribute);
                     if (_typeMapping.ContainsKey(key))
                     {
+                        //если уже в маппинге зарегистрирован тип данных с обработчиками
+                        //то  в список добавляется еще один обработчик на тот же ключевой тип
                         var list = _typeMapping[key];
                         list.Add(item);
                     }
                     else
                     {
-                        _typeMapping.Add(key, new List<Tuple<Type, RemoteEventHandlerAttribute>>(new[] { item }));
+                        //или же создается новая запись в Dictionary с новым ключем по типу данных
+                        _typeMapping.Add(key, new List<(Type handlerType, RemoteEventHandlerAttribute attribute)>(new[] { item }));
                     }
              }
         }
 
         public void HandleEvent(RemoteEventArgs eventArgs)
         {
-            var key = eventArgs.EventData.Type;
+            var dataType = eventArgs.EventData.Type;
             //TODO Корректно обработать ситуацию с Key = null
-            if (key != null && _typeMapping.ContainsKey(key))
+            //Находим в маппинге наборы хендлеров для указанного типа данных в сообщении
+            if (dataType != null && _typeMapping.ContainsKey(dataType))
             {
-                var tuples = _typeMapping[key].OrderBy(p => p.Item2.Order).ToList();
-                foreach (var tuple in tuples)
+                //Достаем все хендлеры с соответсвующими им трибутами
+                var handlerPairs = _typeMapping[dataType].OrderBy(p => p.Item2.Order).ToList();
+
+                //Запускаются все обработчики если они соответсвуют топику сообщения
+                foreach (var handlerPair in handlerPairs)
                 {
-                    if (tuple.Item2.OnlyHandleThisTopic && eventArgs.Topic != tuple.Item2.ForTopic)
+                    //Если зарегистрированные хендлеры не подходят под топик сообщения
+                    if (handlerPair.Item2.OnlyHandleThisTopic && eventArgs.Topic != handlerPair.Item2.ForTopic)
                     {
                         continue;
                     }
 
                     try
                     {
-                        var handler = (IRemoteEventHandler)_iocResolver.Resolve(tuple.Item1);
+                        ///инстанцируется по типу хендлера 
+                        var handler = _iocResolver.Resolve<IRemoteEventHandler>(handlerPair.handlerType);
                         handler.HandleEvent(eventArgs);
                     }
                     catch (Exception ex)
@@ -80,7 +106,7 @@ namespace Abp.RemoteEventBus.Impl
 
                         _eventBus.Trigger(this, new RemoteEventHandleExceptionData(ex, eventArgs));
 
-                        if (tuple.Item2.SuspendWhenException)
+                        if (handlerPair.Item2.SuspendWhenException)
                         {
                             eventArgs.Suspended = true;
                         }
